fed1b783ea74e1947f9b592e4ac44422389bdbf7
MADAM MASK MD <146187447+MADAM-MASK-OFFICIAL@users.noreply.github.com>
2023-10-30 12:55:46 -0500

    👸 𝕸𝕬𝕯𝕬𝕸 𝕸𝕬𝕾𝕶 𝕸𝕯 𝖁2 👸

--- commands/_help.js
+++ commands/_help.js
@@ -24,7 +24,7 @@ Secktor.cmd({
             alias: ["menu"],
             desc: "Help list",
             category: "general",
-            react: "✨",
+            react: "🗂️",
             filename: __filename
         },
         async(Void, citel, text) => {
